91bbe1a9f45c0d4cf9e01ded6bf8a00534687066
sico henricks <sicohenricks@hotmail.com>
2020-04-01 19:39:33 +0200

    Add project files.

---
+++ DaisyMei/Bot.cs
@@ -0,0 +1,31 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaisyMei
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var config = new DiscordConfiguration
+            {
+
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+        }
+
+        private Task OnClientReady(ReadyEventArgs e)
+        {
+            return null;
+        }
+    }
+}
---
+++ DaisyMei/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DaisyMei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+        }
+    }
+}
